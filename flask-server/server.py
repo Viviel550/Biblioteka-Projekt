6253753d5041b076b2889bf9a684fe35491e1bf6
bartos4g4 <162725041+bartos4g4@users.noreply.github.com>
2025-04-02 19:10:56 +0200

    rozdzielenie na osobne pliki

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,12 +1,14 @@
 from flask import Flask, jsonify, request, session
 import requests, logging, sys, os
 from blueprints.login import auth
+from blueprints.categories import categories_bp
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 app = Flask(__name__)
 app.secret_key = 'lubiekotki123'
 
-# register the login blueprint
-app.register_blueprint(auth)  
+# Register blueprints
+app.register_blueprint(auth)
+app.register_blueprint(categories_bp)  
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -27,45 +29,6 @@ def testbooks():
             logging.error(f"Failed to fetch books for author {author}")
 
     logging.info(f"Total books fetched: {len(books)}")
-    return jsonify(books)
-
-@app.route('/genres')
-def GetGenres():
-    genres = []
-    response = requests.get(f'https://wolnelektury.pl/api/genres/')
-    if response.status_code == 200:
-        genresResponse = response.json()
-        genres.extend(genresResponse)
-        logging.info(f"Fetched {len(genres)} genres")
-    else:
-        logging.error(f"Failed to fetch genres")
-
-    return jsonify(genres)
-
-@app.route('/sendGenre', methods=['POST'])
-def sendGenre():
-    data = request.get_json()
-    genreName = data.get('genre')
-
-    if not genreName:
-        return jsonify({'error': 'Brak gatunku'}), 400
-
-    session['current_genre'] = genreName
-    return jsonify({'message': f'Zapisano gatunek: {genreName}'}), 200
-
-
-@app.route('/genreBooks')
-def genreBooks():
-    genreName = session.get('current_genre')
-    books = []
-    logging.info(f"https://wolnelektury.pl/api/genres/{genreName}/books/")
-    response = requests.get(f'https://wolnelektury.pl/api/genres/{genreName}/books/')
-    if response.status_code == 200:
-        genre_books = response.json()
-        logging.info(f"Fetched {len(genre_books)} books for genre {genreName}")
-        books.extend(genre_books)
-    else:
-        logging.error(f"Failed to fetch books for genre {genreName}")
     return jsonify(books)
 
 
