c592da0a2d3a7b14ff34a869c25c4cd599a55ae2
bartos4g4 <162725041+bartos4g4@users.noreply.github.com>
2025-04-03 12:21:53 +0200

    update layout

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -9,21 +9,31 @@ import '../styles/Categories.css';
 
 function GetGenres() {
   const [genres, setGenres] = useState([]);
-  const [selectedGenre, setSelectedGenre] = useState(null);
+  const [selectedGenre, setSelectedGenre] = useState('artykul');
   const [books, setBooks] = useState([]);
   const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   // Pobieranie gatunków
   useEffect(() => {
       fetch('/genres')
           .then(response => response.json())
-          .then(data => setGenres(data))
+          .then((data) => {
+            setGenres(data); 
+            const defaultGenre = data.find((genre) => genre.name === 'Artykuł');
+            if (defaultGenre) {
+              setSelectedGenre(defaultGenre.slug); 
+            }
+          })
           .catch(error => handleError(error));
   }, []);
 
   // Pobieranie książek przy zmianie gatunku
   useEffect(() => {
       if (selectedGenre) {
+        setIsLoading(true); 
+        setBooks([]); 
+
           fetch('/sendGenre', {
               method: 'POST',
               headers: { 'Content-Type': 'application/json' },
@@ -31,8 +41,16 @@ function GetGenres() {
           })
           .then(() => fetch('/genreBooks'))
           .then(response => response.json())
-          .then(data => setBooks(data.slice(0, 50)))
-          .catch(error => handleError(error));
+          .then((data) => {
+            //setBooks(data.slice(0, 18));
+            setBooks(data);
+            setIsLoading(false); // Koniec ładowania
+          })
+          .catch((error) => {
+            handleError(error);
+            setIsLoading(false); // Koniec ładowania nawet przy błędzie
+          });
+          
       }
   }, [selectedGenre]);
 
@@ -52,23 +70,31 @@ function GetGenres() {
 
   return (
       <div className="main">
-          <div className="collection">
-              <h3>Dostępne gatunki</h3>
+          <div className="collectionCategories">             
               <div className="genres">
-                {genres.map((genre, index) => (
-                    <div key={index} className="genre">
-                        <button onClick={() => handleGenreClick(genre.slug)}>
-                            {genre.name}
-                        </button>
-                    </div>
-                ))}
+                <div className='genres-title'>
+                  <h1>Dostępne gatunki</h1>
+                </div>               
+                <div className='genres-module'>
+                  {genres.map((genre, index) => (
+                      <div key={index} className="genre">
+                          <button onClick={() => handleGenreClick(genre.slug)}>
+                              {genre.name}
+                          </button>
+                      </div>
+                  ))}
+                </div>
               </div>
               {selectedGenre && (
                     <div className="books-section">
                       <h3>Książki z gatunku: {
                         genres.find(g => g.slug === selectedGenre)?.name
                       }</h3>
-                      {books.length > 0 ? (
+                      {isLoading ? (
+                      <div className="loading-message">
+                        Ładowanie książek...
+                      </div>
+                      ) : books.length > 0 ? (
                         <div className="books">
                           {books.map((book, index) => (
                             <div key={index} className="book">
