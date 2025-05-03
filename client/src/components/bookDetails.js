975fd1c3b09d8378a4e732cbc72d8f9a096465c4
bartos4g4 <162725041+bartos4g4@users.noreply.github.com>
2025-05-03 22:22:54 +0200

    dodano opinie oraz średnią ocenę
    funkcjonalności opinii: wyświetlanie, dodawanie, usuwanie
    
    funkcjonalności ocen: wyświetlanie średniej oceny
    
    należy zainstalować komponent do gwiazdkowego ratingu: npm install @mui/material @emotion/react @emotion/styled

--- client/src/components/bookDetails.js
+++ client/src/components/bookDetails.js
@@ -2,14 +2,75 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
 import '../styles/bookDetails.css';
+import Rating from '@mui/material/Rating';
+
+function OpinionPopup({ onClose, onSubmit }) {
+  const [opinion, setOpinion] = useState('');
+  const [rating, setRating] = useState(1);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(opinion, rating);
+    setOpinion('');
+    setRating(1);
+  };
+
+  return (
+    <div className="popup-overlay">
+      <div className="popup-content">
+      <button className="close-btn" onClick={onClose} style={{ padding: 5 }}>X</button>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Ocena:
+            <Rating
+              name="star-rating"
+              value={rating}
+              onChange={(event, newValue) => setRating(newValue < 1 ? 1 : newValue)}
+              max={5}
+              required
+            />
+          </label>
+          <label>
+            <textarea
+              className='popup-textarea'
+              value={opinion}
+              onChange={e => setOpinion(e.target.value)}
+              placeholder="Twoja opinia"
+              required
+            />
+          </label>          
+          <button type="submit">Wyślij</button>
+        </form>
+      </div>
+    </div>
+  );
+}
 
 function BookDetails() {
   const { slug } = useParams();
   const [details, setDetails] = useState({});
   const [error, setError] = useState(false);
+  const [opinions, setOpinions] = useState([]);
+  const [showOpinionPopup, setShowOpinionPopup] = useState(false);
+  const [average, setAverage] = useState(null);
+  const [ratingsCount, setRatingsCount] = useState(0);
 
   const token = localStorage.getItem('token');
 
+  //uzyskanie user_id z tokenu
+  let user_Id_token = null;
+  try {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const decoded = jwtDecode(token);
+      user_Id_token = decoded.user_id;
+    }
+  } catch (e) {
+    user_Id_token = null;
+  }
+
+
+  //pobranie szczegółów
   useEffect(() => {
     fetch('/getBookDetails', {
       method: 'POST',
@@ -34,6 +95,50 @@ function BookDetails() {
     setError(true);
   };
 
+  //pobranie opinii
+  const fetchOpinions = () =>  {
+    if (!slug) return;
+
+    fetch(`/getBookOpinions?slug=${encodeURIComponent(slug)}`)
+      .then(response => response.json())
+      .then((data) => {
+        setOpinions(data);
+        console.log('Otrzymane opinie:', data);
+      })
+      .catch(error => {
+        handleError(error);
+      });
+  };
+  //odświeżenie przy dodaniu lub usunięciu opini
+  useEffect(() => {
+    if (!slug) return;
+    fetchOpinions();
+    fetchAvarage();
+  }, [slug]);
+
+  //pobranie średniej oceny
+  const fetchAvarage = () =>  {
+    if (!slug) return;
+
+    fetch(`/getAvgRating?slug=${encodeURIComponent(slug)}`)
+      .then(response => response.json())
+      .then((data) => {
+        if (data && data.length > 0) {
+          setAverage(data[0].avarage);
+          setRatingsCount(data[0].count);
+        } else {
+          setAverage(null);
+          setRatingsCount(0);
+        }
+        console.log('Otrzymane oceny:', data);
+      })
+      .catch(error => {
+        handleError(error);
+      });
+  };
+
+
+
   //dodanie do ulubionych
   const handleClickFavorites = (slug) => {
     if (!token) {
@@ -53,6 +158,51 @@ function BookDetails() {
     .then(data => alert(data.message || data.error));
   };
 
+  //dodanie opini
+  const handleClickAddOpinion = (slug) => {
+    if (!token) {
+      alert('Aby dodać opinię musisz się zalogować!');
+      return;
+    }
+    setShowOpinionPopup(true);
+  };
+  const handleOpinionSubmit = (opinionText, rating) => {
+    fetch('/addOpinion', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ slug, opinionText, rating })
+    })
+    .then(response => response.json())
+    .then(data => {
+      alert(data.message || data.error);
+      setShowOpinionPopup(false);
+      fetchOpinions();
+      fetchAvarage();
+    })
+  };
+
+  //usunięcie opini
+  const handleDeleteOpinion = (review_id) => {
+    if (!window.confirm('Czy na pewno chcesz usunąć tę opinię?')) return;
+    fetch('/deleteOpinion', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ review_id })
+    })
+    .then(res => res.json())
+    .then(data => {
+      alert(data.message || data.error);
+      fetchOpinions();
+      fetchAvarage();
+    })
+    .catch(err => alert('Błąd podczas usuwania opinii'));
+  };
 
   if (error) {
       return <div className="errorCategories"><h1>Strona Tymczasowo Niedostępna</h1></div>;
@@ -80,6 +230,11 @@ function BookDetails() {
               Tłumaczenie: {details.translators.map(t => t.name).join(', ')}
             </h3>
           )}
+          <h2 className="details-avarage">
+            {average !== null
+              ? `Średnia ocen: ${average} (${ratingsCount} głosów)`
+              : 'Brak ocen'}
+          </h2>
           {details.description && (
             <div className="details-description">
               <h4>Opis:</h4>
@@ -96,9 +251,45 @@ function BookDetails() {
           </div>
         </div>
       </div>
-      <div className='details-opinions'>
-          <h3>Opinie</h3>
+      <div className="opinions-section">
+        <h3 className="opinions-title">Opinie</h3>
+        <div className='opinions-add'>
+          <button onClick={() => handleClickAddOpinion(slug)}>Dodaj opinie</button>
+        </div>
+        {opinions.length === 0 ? (
+          <h3 className="opinions-title">Brak opinii.</h3>
+        ) : (
+          <ul className="opinions-list">
+            {opinions.map(opinion => (
+              <li key={opinion.review_id} className="opinion-item">
+                <div className="opinion-header">
+                  <span><strong>Autor:</strong> {opinion.nickname}</span>
+                  <span><strong>Ocena:</strong> {opinion.rating} / 5</span>
+                  <span><strong>Data:</strong> {opinion.review_date ? new Date(opinion.review_date).toLocaleDateString() : ""}</span>
+                </div>
+                <div className="opinion-content">
+                  {opinion.review}
+                </div>
+                {user_Id_token && opinion.user_id === user_Id_token && (
+                  <button
+                    className="delete-opinion-btn"
+                    onClick={() => handleDeleteOpinion(opinion.review_id)}
+                    style={{padding: 5}}
+                  >
+                    Usuń
+                  </button>
+                )}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
+      {showOpinionPopup && (
+        <OpinionPopup
+          onClose={() => setShowOpinionPopup(false)}
+          onSubmit={handleOpinionSubmit}
+        />
+      )}
     </div>
   )
 }
--- flask-server/blueprints/bookDetails.py
+++ flask-server/blueprints/bookDetails.py
@@ -106,10 +106,197 @@ def addToFavorites():
                 """, (user_id, book_id))
 
                 conn.commit()
-                return jsonify({"message": "Książka dodana do ulubionych!"}), 200
-            
-            
+                return jsonify({"message": "Książka dodana do ulubionych!"}), 200           
+    except psycopg.Error as e:
+        return jsonify({"error": str(e)}), 500
+    
+
+#pobierz opinie
+@bookDetails_bp.route('/getBookOpinions', methods=['GET'])
+def getBookOpinions():
+    slug = request.args.get('slug')
+    if not slug:
+        return jsonify({'error': 'Brak slug'}), 400
+    logging.info(f"Received slug for opinions: {slug}")
+
+    try:
+        with psycopg.connect(
+            host=DB_HOST, dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD
+        ) as conn:
+            with conn.cursor() as cur:
+                # Pobierz book_id
+                cur.execute("""
+                    SELECT book_id FROM public."Books" WHERE slug = %s
+                """, (slug,))                
+                row = cur.fetchone()
+                if not row:
+                    logging.info("Brak ksiązki o takim slug")
+                    return jsonify([]), 200
+                book_id = row[0]
+                logging.info(f"Found book_id for opinions: {book_id}")
+
+                # Pobierz opinie
+                cur.execute("""
+                    SELECT * FROM public.book_reviews
+                    WHERE book_id = %s
+                    ORDER BY review_date ASC
+                """, (book_id,))                
+                opinions = cur.fetchall()           
+
+                result = []
+                for opinion in opinions:
+
+                    #pobranie danych autora
+                    user_id = opinion[2]
+                    cur.execute("""
+                    SELECT * FROM public.users
+                    WHERE user_id = %s
+                     """, (user_id,))                
+                    author = cur.fetchone()
+
+                    #dołączenie danych
+                    result.append({
+                        "review_id": opinion[0],                       
+                        "book_id": opinion[1],
+                        "user_id": opinion[2],
+                        "nickname": author[1],
+                        "rating": opinion[3],
+                        "review": opinion[4],
+                        "review_date": opinion[5]
+                    })
+
+                conn.commit()
+                return jsonify(result), 200           
     except psycopg.Error as e:
         return jsonify({"error": str(e)}), 500
 
 
+#dodanie opini o książce
+@bookDetails_bp.route('/addOpinion', methods=['POST'])
+@token_required
+def addOpinion():
+    user_id = request.user_id
+    data = request.get_json()
+    slug = data.get('slug') if data else None
+    opinionText = data.get('opinionText') if data else None
+    rating = data.get('rating') if data else None
+    if not slug:
+        return jsonify({'error': 'Brak slug'}), 400
+    if not opinionText:
+        return jsonify({'error': 'Brak opinionText'}), 400
+    if not rating:
+        return jsonify({'error': 'Brak rating'}), 400
+    
+    logging.info("Received slug, user_id and opinion.")
+
+    try:
+        with psycopg.connect(
+            host=DB_HOST, dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD
+        ) as conn:
+            with conn.cursor() as cur:
+                # Dodaj książkę do Books jeśli nie istnieje
+                cur.execute("""
+                    INSERT INTO public."Books" (slug)
+                    SELECT %s
+                    WHERE NOT EXISTS (
+                        SELECT 1 FROM public."Books" WHERE slug = %s
+                    )
+                """, (slug, slug))
+
+                # Pobierz book_id
+                cur.execute("""
+                    SELECT book_id FROM public."Books" WHERE slug = %s
+                """, (slug,))                
+                row = cur.fetchone()
+                if not row:
+                    return jsonify({"error": "Nie udało się pobrać book_id"}), 500
+                book_id = row[0]
+                logging.info(f"Received book_id: {book_id}")
+
+                # Dodaj opinie
+                cur.execute("""
+                    INSERT INTO public.book_reviews (book_id, user_id, rating, review, review_date)
+                    VALUES (%s, %s, %s, %s, CURRENT_DATE)
+                """, (book_id, user_id, rating, opinionText))
+
+                conn.commit()
+                return jsonify({"message": "Opinia dodana!"}), 200           
+    except psycopg.Error as e:
+        return jsonify({"error": str(e)}), 500
+    
+#usunięcie opini o książce
+@bookDetails_bp.route('/deleteOpinion', methods=['DELETE'])
+@token_required
+def deleteOpinion():
+    user_id = request.user_id
+    data = request.get_json()
+    review_id = data.get('review_id') if data else None
+    if not review_id:
+        return jsonify({'error': 'Brak review_id'}), 400
+    
+    logging.info(f"Received review_id: {review_id} for delete, by user: {user_id}.")
+
+    try:
+        with psycopg.connect(
+            host=DB_HOST, dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD
+        ) as conn:
+            with conn.cursor() as cur:
+                # usunięcie opini
+                cur.execute("""
+                    DELETE FROM public.book_reviews 
+                    WHERE review_id = %s 
+                    AND user_id = %s;
+                """, (review_id, user_id))
+
+                if cur.rowcount == 0:
+                    return jsonify({"error": "Nie znaleziono opinii lub brak uprawnień"}), 404
+
+                conn.commit()
+                return jsonify({"message": "Opinia usunięta!"}), 200           
+    except psycopg.Error as e:
+        return jsonify({"error": str(e)}), 500
+    
+#pobierz średnią
+@bookDetails_bp.route('/getAvgRating', methods=['GET'])
+def getAvgRating():
+    slug = request.args.get('slug')
+    if not slug:
+        return jsonify({'error': 'Brak slug'}), 400
+    logging.info(f"Received slug for avg rating: {slug}")
+
+    try:
+        with psycopg.connect(
+            host=DB_HOST, dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD
+        ) as conn:
+            with conn.cursor() as cur:
+                # Pobierz book_id
+                cur.execute("""
+                    SELECT book_id FROM public."Books" WHERE slug = %s
+                """, (slug,))                
+                row = cur.fetchone()
+                if not row:
+                    logging.info("Brak ksiązki o takim slug")
+                    return jsonify([]), 200
+                book_id = row[0]
+                logging.info(f"Found book_id for opinions: {book_id}")
+
+                # Pobierz średnią
+                cur.execute("""
+                    SELECT 
+                        ROUND(AVG(rating), 2) AS average_rating,
+                        COUNT(rating) AS ratings_count
+                    FROM public.book_reviews
+                    WHERE book_id = %s
+                """, (book_id,))                
+                avarage = cur.fetchone() 
+
+                avarageArray = []
+                avarageArray.append({
+                        "avarage": avarage[0],                       
+                        "count": avarage[1],
+                    })          
+
+                conn.commit()
+                return jsonify(avarageArray), 200           
+    except psycopg.Error as e:
+        return jsonify({"error": str(e)}), 500
