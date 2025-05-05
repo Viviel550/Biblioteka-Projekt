ff074f5ba8ef3647b7b1376f15bf3e34d1d3a650
Viviel550 <272851@student.pwr.edu.pl>
2025-05-06 00:10:43 +0200

    poprawa

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -35,7 +35,7 @@ function Header() {
           <a href="/autorzy" className="nav-item">Autorzy</a>
           
           <div className="auth-buttons">
-            {token && (userId || workerId) ? (
+            {token ? (
               <a href="/Panel" className="nav-item profile-btn">
                 <span className="profile-icon">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
